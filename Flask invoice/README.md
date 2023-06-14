a73f84784b91080538d9eb340a6c4ad270e7f5a7
Nevin V Adatte <nevinvadatte@gmail.com>
2023-06-14 21:29:44 +0530

    Flask invoice added

---
+++ Flask invoice/README.md
@@ -0,0 +1,20 @@
+
+# Invoice Generator
+
+This is a simple program to generate invoice
+
+>Required library for the execution
+
+```
+
+pip install reportlab
+
+```
+
+>Run using this command
+
+```
+
+py invoice.py
+
+```
---
+++ Flask invoice/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/sign')
+def sign():
+    return render_template("sign.html")
+
+if __name__ == '__main__':
+    app.run()
